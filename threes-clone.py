16531eac3aea94bf57c2c997a08c3dbae6a89e9c
you <you@example.com>
2018-07-20 21:43:11 -0700

    modify functions to work with directions no need for board rotation

--- threes-clone.py
+++ threes-clone.py
@@ -21,7 +21,7 @@ WHITE = (220,220,220)
 BLUE = (85, 153, 255)
 RED = (251, 89, 154)
 BLACK = (0,0,0)
- 
+
 
 BGCOLOUR = DARKGRAY
 EMPTYTILECOLOUR = LIGHTGRAY
@@ -53,26 +53,13 @@ def main():
 				sys.exit()
 			elif event.type == KEYDOWN: # key was pressed
 				if event.key == K_UP or event.key == K_w:
-					slideBoard(mainBoard)
+					slideBoard(mainBoard, "up")
 				elif event.key == K_LEFT or event.key == K_a:
-					rotateBoard(mainBoard)
-					rotateBoard(mainBoard)
-					rotateBoard(mainBoard)
-					slideBoard(mainBoard)
-					rotateBoard(mainBoard)
+					slideBoard(mainBoard, "left")
 				elif event.key == K_DOWN or event.key == K_s:
-					rotateBoard(mainBoard)
-					rotateBoard(mainBoard)
-					slideBoard(mainBoard)
-					rotateBoard(mainBoard)
-					rotateBoard(mainBoard)
+					slideBoard(mainBoard, "down")
 				elif event.key == K_RIGHT or event.key == K_d:
-					rotateBoard(mainBoard)
-					slideBoard(mainBoard)
-					rotateBoard(mainBoard)
-					rotateBoard(mainBoard)
-					rotateBoard(mainBoard)
-
+					slideBoard(mainBoard, "right")
 		pygame.display.update()
 		FPSCLOCK.tick(FPS)
 
@@ -126,28 +113,63 @@ def getTopLeftCorner(tilex, tiley): # gets pixel coords of tile in specified row
 	y = tiley * (TILEHEIGHT + GAPSIZE) + YMARGIN
 	return x, y
 
-def slideBoard(board): # slides the tiles up
+def slideBoard(board, direction): # slides the tiles up
 	moved = []
-	for tilex in range(BOARDSIZE):
-		for tiley in range(1, BOARDSIZE):
-			moveValidity = checkValidity(board, tilex, tiley) 
+	if direction == 'up' or direction == "left":
+		start = 0
+		stop = BOARDSIZE
+		increment = 1
+	elif direction == 'down' or direction == 'right': # for these directions, the loop needs to go in reverse
+		start = BOARDSIZE
+		stop = -1
+		increment = -1
+		
+	for tilex in range(start, stop, increment):
+		for tiley in range(start, stop, increment):
+			# get coordinate of tile being moved into based on direction
+			if direction == "up":
+				targetTilex = tilex
+				targetTiley = tiley - 1
+			elif direction == "left":
+				targetTilex = tilex - 1
+				targetTiley = tiley
+			elif direction == "down":
+				targetTilex = tilex
+				targetTiley = tiley + 1
+			elif direction == "right":
+				targetTilex = tilex + 1
+				targetTiley = tiley
+				
+			moveValidity = checkValidity(board, tilex, tiley, targetTilex, targetTiley) 
 			if  moveValidity == 1: # valid move, two numbers that are the same
-				board[tilex][tiley - 1] += 1
+				board[targetTilex][targetTiley] += 1
 				board[tilex][tiley] = 0
 			elif moveValidity == 2: # valid move, combining a one and a two
-				board[tilex][tiley - 1] = 1
+				board[targetTilex][targetTiley] = 1
 				board[tilex][tiley] = 0
 			elif moveValidity == 0: # valid move, moving into empty space
-				board[tilex][tiley - 1] = board[tilex][tiley]
+				board[targetTilex][targetTiley] = board[tilex][tiley]
 				board[tilex][tiley] = 0
 			if moveValidity >= 0:
-				moved.append(tilex)
-	addNewTile(board, moved)
+				moved.append((tilex, tiley))
+	addNewTile(board, moved, direction)
 
-def addNewTile(board, moved):
+def addNewTile(board, moved, direction):
 	if len(moved) > 0:
-		tilex = random.choice(moved)
-		board[tilex][3] = randomTile(board)
+		tile = random.choice(moved)
+		if direction == 'up':
+			tilex = tile[0]
+			tiley = 3
+		elif direction == 'left':
+			tilex = 3
+			tiley = tile[1]
+		elif direction == 'down':
+			tilex = tile[0]
+			tiley = 0
+		elif direction == 'right':
+			tilex = 0
+			tiley = tile[1]
+		board[tilex][tiley] = randomTile(board)
 				
 def randomTile(board):
 	choices = [ONE,TWO,THREE,THREE]
@@ -159,20 +181,11 @@ def randomTile(board):
 				choices.append(TWO)	
 	return random.choice(choices)
 
-def rotateBoard(board): # rotates the board 90 degrees counterclockwise
-	# make a copy of the board
-	copy = [[0 for x in range(BOARDSIZE)] for y in range(BOARDSIZE)]
-	for x in range(BOARDSIZE): 
-		for y in range(BOARDSIZE):
-			copy[x][y] = board[x][y]
-
-	for x in range(BOARDSIZE): # rotate the board
-		for y in range(BOARDSIZE):
-			board[y][BOARDSIZE - x - 1] = copy[x][y]
-
-def checkValidity(board, tilex, tiley): # 1 = success, -1 = no success, 2 = success pairing 1 and 2, 0 = moving into an empty tile
+def checkValidity(board, tilex, tiley, targetTilex, targetTiley): # 1 = success, -1 = no success, 2 = success pairing 1 and 2, 0 = moving into an empty tile
+	if targetTilex == -1 or targetTiley == -1 or targetTilex >= BOARDSIZE or targetTiley >= BOARDSIZE:
+		return -1
 	tileOne = board[tilex][tiley]
-	tileTwo = board[tilex][tiley - 1]
+	tileTwo = board[targetTilex][targetTiley]
 	if tileOne == tileTwo and tileOne >= 1:
 		return 1
 	elif (tileOne == -1 and tileTwo == -2) or (tileOne == -2 and tileTwo == -1):
@@ -184,5 +197,5 @@ def checkValidity(board, tilex, tiley): # 1 = success, -1 = no success, 2 = succ
 
 main()
 
-          
+		  
 
